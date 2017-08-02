b706087d029b07a456f8b1be18d53ea758946edb
Jason Aden <jasonaden@google.com>
2017-08-02 15:23:35 -0700

    Pub esm5 version

---
+++ observable/onErrorResumeNext.js
@@ -0,0 +1,3 @@
+import { onErrorResumeNextStatic } from '../operator/onErrorResumeNext';
+export var onErrorResumeNext = onErrorResumeNextStatic;
+//# sourceMappingURL=onErrorResumeNext.js.map
---
+++ observable/race.js
@@ -0,0 +1,3 @@
+import { raceStatic } from '../operator/race';
+export var race = raceStatic;
+//# sourceMappingURL=race.js.map
